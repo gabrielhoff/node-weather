08f9c6eb0451b19d0a40fdea5375c10689f6210c
Gabriel Hoff <gabriel000@gmail.com>
2019-07-17 20:59:52 -0400

    Remove blank line

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,6 @@ window.onload = () => {
 
     weatherForm.addEventListener('submit', (e) => {
         e.preventDefault();
-
         const location = search.value;
         messageOne.textContent = 'Loading...'
         fetch('http://localhost:3000/weather?address=' + location).then((response) => {
